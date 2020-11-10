dacfea4c8a91c8bab74bade413c5376304bb4c47
Ossi Miilukangas <ossi.miilukangas@hotmail.com>
2020-11-10 16:31:18 +0200

    User endpoints now integrated with database

--- API/index.js
+++ API/index.js
@@ -1,16 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
+const { v4: uuidv4 } = require('uuid');
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const jwtStrategy = require("passport-jwt").Strategy,
   ExtractJwt = require("passport-jwt").ExtractJwt;
 const jwtSecretKey = require("./jwt-key.json");
 const e = require("express");
+const mysql = require("mysql");
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
+// Create MySQL connection
+var db = mysql.createConnection({
+  host: "group3db.czavswxxt8w0.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "adminroot",
+  database: "group3db"
+})
+
+db.connect();
+
+/* EXAMPLE QUERIES:
+app.get("/mysql", (req, res) => {
+  let sql = "SELECT * FROM user";
+  db.query(sql, function(err, data, fields) {
+    if (err) throw err;
+    res.status(200).json({ data })
+  })
+});
+
+app.post("/mysql", (req, res) => {
+  let sql = "INSERT INTO user(userName, password) VALUES (?)";
+  let values = [
+    req.body.username,
+    req.body.password
+  ];
+  db.query(sql, [values], function(err, data, fields) {
+    if (err) throw err;
+    res.status(200).send("toimii");
+  })
+})*/
 
 // Object arrays for development before database
 let users = [
@@ -73,47 +106,66 @@ app.post("/user/register", (req, res) => {
     return;
   }
 
-  //Test that username is not taken
-  if (users.find((e) => e.username == req.body.username) !== undefined) {
-    res.status(409).send("Conflict: Username already taken");
-    return;
-  }
-
-  // hash the password
-  const hashedPassword = bcrypt.hashSync(req.body.password, 6);
-
-  const newUser = {
-    id: users.length + 1,
-    username: req.body.username,
-    email: req.body.email,
-    password: hashedPassword,
-  };
-  users.push(newUser);
-
-  res.status(201).json({ newUser });
-});
-
-app.get("/user/login",
-  (req, res) => {
-    const user = users.find((e) => e.username == req.body.username);
-
-    // if user not found
-    if (user === undefined) {
-      res.status(401).send("Unauthorized: Username not found");
+  // Test that username is not taken
+  let sql_f = "SELECT * FROM user WHERE userName = '" + req.body.username + "'";
+  db.query(sql_f, function(err, data, fields) {
+    if (err) {
+      console.log(err);
+      res.status(500).send("MySQL ERROR");
       return;
     }
 
+    // If user was found with the username
+    if (data.length > 0) {
+      res.status(409).send("Conflict: Username already taken");
+      return;
+    }
+
+    // New query to insert user into database
+    let sql_i = "INSERT INTO user(userName, password) VALUES (?)";
+    const hashedPassword = bcrypt.hashSync(req.body.password, 6);
+    let values = [
+      req.body.username,
+      hashedPassword
+    ];
+    db.query(sql_i, [values], function(err, data, fields) {
+      if (err) {
+        console.log(err);
+        res.status(500).send("MySQL ERROR");
+        return;
+      }
+      res.status(200).send({ username: req.body.username, password: hashedPassword });
+    })
+  })
+});
+
+app.get("/user/login", (req, res) => {
+  // find user from database
+  let sql_f = "SELECT * FROM user WHERE userName = '" + req.body.username + "' LIMIT 1";
+  db.query(sql_f, function(err, data, fields) {
+    if (err) {
+      console.log(err);
+      res.status(500).send("MySQL ERROR");
+      return;
+    }
+
+    // if user not found
+    if (data.length === 0) {
+      res.status(401).send("Unauthorized: Username not found");
+      return;
+    }
+  
+    let dbuser = data[0];
     // compare passwords
-    if (bcrypt.compareSync(req.body.password, user.password) == false) {
+    if (bcrypt.compareSync(req.body.password, dbuser.password) == false) {
       res.status(401).send("Unauthorized: Wrong password");
       return;
     }
 
     // construct body and set options
     const body = {
-      id: user.id,
-      username: user.username,
-      email: user.email,
+      id: dbuser.userid,
+      username: dbuser.username,
     };
 
     const payload = {
@@ -126,35 +178,57 @@ app.get("/user/login",
 
     // create and return token
     const token = jwt.sign(payload, jwtSecretKey.secret, options);
-    res.status(200).json({ token });
-  }
-);
+    res.status(200).send({ userId: dbuser.userid, token: token });
+  })
+});
 
 app.delete("/user/id/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    // find index of a json object from resources by id
-    const result = users.findIndex((e) => e.id == req.params.id);
+    // find user from database by id
+    let sql_f = "SELECT * FROM user WHERE userid = '" + req.params.id + "' LIMIT 1";
+    db.query(sql_f, function(err, data, fields) {
+      if (err) {
+        console.log(err);
+        res.status(500).send("MySQL ERROR");
+        return;
+      }
 
-    // test that index was found
-    if (result === -1) {
-      res.status(404).send("User Id Not Found");
-      return;
-    }
+      // test that user was found
+      if (data.length === 0) {
+        res.status(404).send("User Id Not Found");
+        return;
+      }
 
-    // test that the user is authorized to modify the resource
-    if (users[result].id !== req.user.id) {
-      res.status(403).send("Forbidden: User not authorized");
-      return;
-    }
-    users.splice(result, 1);
-    res.status(200).send("User deleted, Id: " + req.params.id);
+      let dbuser = data[0]
+      // test that the user is authorized to modify the resource
+      if (dbuser.userid !== req.user.id) {
+        res.status(403).send("Forbidden: User not authorized");
+        return;
+      }
+
+      // delete user from database
+      let sql_d = "DELETE FROM user WHERE userid = " + req.params.id;
+      db.query(sql_d, function(err, data, fields) {
+        if (err) {
+          console.log(err);
+          res.status(500).send("MySQL ERROR");
+          return;
+        }
+
+        res.status(200).send("User deleted, Id: " + req.params.id);
+      })
+    })
   }
 );
 
 // get users for testing
 app.get("/user", (req, res) => {
-  res.json({ users });
+  let sql = "SELECT * FROM user";
+  db.query(sql, function(err, users, fields) {
+    if (err) throw err;
+    res.status(200).json({ users })
+  })
 });
 
 /*********************************************
